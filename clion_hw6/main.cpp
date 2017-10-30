e46a004222c99b8c6cfb7c12b4a0fd5ce9e4c861
k3x1bq <k3x1bq@gmail.com>
2017-10-30 21:38:59 +0300

    polymorph_add_checking

--- clion_hw6/main.cpp
+++ clion_hw6/main.cpp
@@ -8,13 +8,19 @@ using namespace std;
 
 int main(int argc,char **argv) {
     ifstream input(*++argv);
-    shape** mas;
-    int size = create_mas_from_file(input,mas);
-    for (int i = 0; i < size; ++i) {
-        mas[i]->show();
-        cout << "area = "<<mas[i]->area() << endl;
-        cout << "volume = " << mas[i]->volume() << endl<<endl;
+    shape **mas;
+    int size;
+    if (input.is_open()) {
+        size = create_mas_from_file(input, mas);
+        for (int i = 0; i < size; ++i) {
+            mas[i]->show();
+            cout << "area = " << mas[i]->area() << endl;
+            cout << "volume = " << mas[i]->volume() << endl << endl;
+        }
+    } else {
+        cout << "Can't open file" << endl;
     }
-    free_mas(mas,size);
+    free_mas(mas, size);
+    input.close();
     return 0;
 }
