8247d8e17dc7bc2b5f925406e22a710d4ed1c794
k3x1bq <k3x1bq@gmail.com>
2017-11-06 02:14:12 +0300

    = and copyconstr

--- multimap/main.cpp
+++ multimap/main.cpp
@@ -4,8 +4,43 @@
 
 using namespace std;
 
+class testcls {
+public:
+    testcls(int a, double b, string h):a(a),b(b),h(h){}
+    testcls(const testcls& a){
+        this->a = a.a;
+        this->b = a.b;
+        this->h = a.h;
+    }
+    int a;
+    double b;
+    string h;
+};
+
 int main() {
+    Multimap<string,int>::element_multimap a("blablabla", 123);
     Multimap<string,int> test("HEh", 5);
+    Multimap<string,int> test2;
+   // cout << "test2: " << test2 << endl<< endl;
+    test2.add("asdasda", 6);
+    test2.add("asdasd",4);
+    test2.add("aaa",123);
+    test2.add("aaa",152);
+    test2.show();
+    test2.remove("asdasd");
+    cout << endl;
+    test2.show();
+    cout << endl;
+    Multimap<string,int>::element_multimap* p = test2.find("aaa1");
+    p->show();
+    cout << *p << endl;
+    cout << endl;
+
+
+    Multimap<string,int> test3;
+    test3 += test2;
+    test3.show();
+    cout << endl;
     //test.show();
     list<string> l1;
     l1.add("hah");
@@ -16,6 +51,21 @@ int main() {
     l1.show();
     l1.remove("heh");
     l1.show();
+    list<string> l2(l1); //ошибка конструктора копирования
+    //l2 = l1;
+    l2.show();
+//    list<string> l3= l1;
+//    cout << "l3: " << endl;
+//    l3.show();
+    //list<string> l2 = l1;
+    //l2.show();
    // cout << l1.find("heh") << endl;
+    //testcls t1(5,3.14,"asda");
+   // testcls t2(12,32.2,"gsdsdsd");
+   // list<testcls> l2;
+//
+   // l2.add(t1);
+   // l2.add(t2);
+  //  l2.show();
     return 0;
 }
--- multimap/multimap.hpp
+++ multimap/multimap.hpp
@@ -14,7 +14,69 @@ public:
             head = tmp;
         }
     }
-    void add(V elem){
+//    list(const list& other){
+//        head = NULL;
+//        element* p = other.head;
+//
+//        while (p != NULL){
+//            this->add(*(p->data));
+//            //std::cout<<"oke " << *(this->head->data) <<std::endl;
+//            p = p->next;
+//        }
+//    }
+    list(const list& other){
+        head = NULL;
+        element* p = other.head;
+        try {
+            if (p == NULL)
+                throw 1;
+            element *last = NULL;
+            while (last != p) {
+                while (p->next != last)
+                    p = p->next;
+                this->add(*(p->data));
+                last = p;
+                p = other.head;
+            }
+        } catch(int a){
+            if (a == 1){
+                head = NULL;
+            }
+        }
+    }
+//    list& operator=(const list& other){
+//        while (head != NULL){
+//            element* tmp = head->next;
+//            delete head;
+//            head = tmp;
+//        }
+//        element* p = other.head;
+//        while (p != NULL){
+//            this->add(*(p->data));
+//            p = p->next;
+//        }
+//        return *this;
+//    }
+    list& operator= (const list& other){
+        while (head != NULL){
+            element* tmp = head->next;
+            delete head;
+            head = tmp;
+        }
+        element* p = other.head;
+        if (p==NULL)
+            return *this;
+        element* last = NULL;
+        while (last != p){
+            while (p->next != last)
+                p = p->next;
+            this->add(*(p->data));
+            last = p;
+            p = other.head;
+        }
+        return *this;
+    }
+    void add(const V& elem){
         element* tmp = new element(elem, head);
        // tmp->data = new V(elem);
         //tmp->next = head;
@@ -29,7 +91,7 @@ public:
         }
         return NULL;
     }
-    void remove(V elem){
+    void remove(const V& elem){
         element* p = head;
         if (p != NULL && *(p->data) == elem) {
             head = p->next;
@@ -44,7 +106,7 @@ public:
                 p = p->next;
         }
     }
-    void show(){
+    void show() const {
         element* tmp = head;
         while (tmp != 0){
             //tmp->data->show_all();
@@ -53,7 +115,7 @@ public:
         }
         std::cout << std::endl;
     }
-private:
+//private:
     class element{
     public:
         element() : data(NULL), next(NULL){}
@@ -75,27 +137,141 @@ private:
 template <class K,class V>
 class Multimap {
 public:
+    class element_multimap;
+    Multimap(){}
     Multimap(K key, V val){
-        mas = new element_multimap(key, val);
+       // mas = new list<element_multimap>();
+        mas.add(element_multimap(key, val));
     }
 
     ~Multimap() {
-        delete mas;
+        //delete mas;
     }
 
+    void add(const K& key,const V& value){
+        if (mas.head == NULL){
+            mas.add(element_multimap(key, value));
+            return;
+        }
+        auto p = mas.head;
+        while (p != NULL){
+            if ((p->data->key) == key) {
+                p->data->val.add(value);
+                return;
+            }
+            p = p->next;
+        }
+        mas.add(element_multimap(key, value));
+    }
 
-private:
+    void remove (const K& key){
+        auto p = mas.head;
+        while (p != NULL){
+            if (p->data->key == key){
+                mas.remove(*(p->data));
+            }
+            p = p->next;
+        }
+    }
+    element_multimap* find(const K& key){
+        auto p = mas.head;
+        try {
+            while (p != NULL) {
+                if (p->data->key == key) {
+                    return p->data;
+                }
+                p = p->next;
+            }
+            throw 1;
+        } catch(int a) {
+            if (a == 1){
+                std::cout << "no such key in map" << std::endl;
+                return NULL;
+            }
+
+        }
+       // return NULL;
+    }
+    void show() const {
+        auto p = mas.head;
+        while (p!=NULL) {
+            std::cout << p->data->key << " :";
+            p->data->val.show();
+            p = p->next;
+        }
+    }
+    friend std::ostream& operator << (std::ostream& os, Multimap const& obj){
+        auto p = obj.mas.head;
+        try {
+            if (p == NULL)
+                throw 1;
+            while (p != NULL) {
+                os << p->data->key << " :";
+                p->data->val.show();
+                p = p->next;
+            }
+            return os;
+        } catch (int a){
+            if (a == 1)
+                return os;
+        }
+    }
+    Multimap& operator+=(Multimap& other){
+        auto p = mas.head;
+        try {
+            if (p == NULL)
+                throw 1;
+            while (p->next != NULL)
+                p = p->next;
+            p->next = other.mas.head;
+            other.mas.head = this->mas.head;
+            return *this;
+        } catch (int a){
+            if (a == 1){
+                this->mas = other.mas;
+            }
+            return *this;
+        }
+    }
+//private:
     class element_multimap{
     public:
+        element_multimap(const element_multimap& other){
+            this->key = other.key;
+            this->val = other.val;
+        }
         element_multimap(K key, V value): key(key){
             this->val.add(value);
+        }
+        ~element_multimap(){
+
+        }
+        friend bool operator==(const element_multimap& e1, const element_multimap& e2){
+            return e1.key == e2.key;
+        }
+        friend std::ostream& operator << (std::ostream& os, element_multimap const& obj){
+            obj.show();
+            return os;
+        }
+        void show() const {
+            try {
+                if (this == NULL)
+                    throw 1;
+                std::cout << key << " : ";
+                val.show();
+            }
+            catch (int a) {
+                if (a == 1)
+                    std::cout << "empty value list" << std::endl;
+            }
         }
         K key;
         list<V> val;
     };
 
-    int cap;
-    element_multimap* mas;
+    list<element_multimap> mas;
 };
+
+
 
 #endif //MULTIMAP_MULTIMAP_HPP
